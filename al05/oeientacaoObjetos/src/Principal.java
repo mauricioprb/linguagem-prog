c0e3114546d228e553ab7e2c4450761d1676611b
mauricioprb <mauriciopereirabraga@gmail.com>
2022-04-12 22:19:59 -0300

    al05

---
+++ al05/oeientacaoObjetos/src/Principal.java
@@ -0,0 +1,26 @@
+
+import javax.swing.JOptionPane;
+
+/*
+Cadastrar um produto e um cliente e 
+exibir os dados cadastrados na tela
+ */
+public class Principal {
+
+    
+    public static void main(String[] args) {
+        String nomeProduto = JOptionPane.showInputDialog("Digite o nome do produto");
+        String fabricanteProduto = JOptionPane.showInputDialog("Digite o nome do fabricante do produto");
+        String nomeCliente = JOptionPane.showInputDialog("Digite o nome do cliente");
+        String emailCliente = JOptionPane.showInputDialog("Digite o email do cliente");
+        
+        System.out.println("Nome do Produto: " + nomeProduto);
+        System.out.println("Fabricante: " + fabricanteProduto);
+        System.out.println("Cliente: " + nomeCliente);
+        System.out.println("Email: " + emailCliente);
+        
+    }
+    
+    
+    
+}
