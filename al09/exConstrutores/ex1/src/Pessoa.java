b1e162c57a8c7e771dcfc1cfc2e9b4767a4bb8bb
mauricioprb <mauriciopereirabraga@gmail.com>
2022-05-24 15:53:02 -0300

    att

--- al09/exConstrutores/ex1/src/Pessoa.java
+++ al09/exConstrutores/ex1/src/Pessoa.java
@@ -1,3 +1,5 @@
+package ex1;
+
 public class Pessoa {
     private String nome;
     private int idade;
---
+++ al09/exConstrutores/ex2/src/App.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class App {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Aluno matriculaNome = new Aluno(13344532, "Joao");
+        System.out.println("Matricula: " + matriculaNome.matricula + "\n" + "Nome: " + matriculaNome.nome);
+
+        Aluno dataNascimento = new Aluno("05/06/2005");
+        System.out.println("Data de nascimento: " + dataNascimento.dataNasc);
+
+        Aluno nomeNascData = new Aluno("Pedro", "08/07/2000", 11454322);
+        System.out.println("Nome: " + nomeNascData.nome + "\n" + "Data de nascimento: " + nomeNascData.dataNasc + "\n" + "Matricula: " + matriculaNome.matricula);
+    }
+}
