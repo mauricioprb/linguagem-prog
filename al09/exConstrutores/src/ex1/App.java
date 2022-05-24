2e6c0cd79ca20590f09a38b03d1252f18dc73b44
mauricioprb <mauriciopereirabraga@gmail.com>
2022-05-23 23:53:10 -0300

    ta

---
+++ al09/exConstrutores/src/ex1/App.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int option;
+
+        System.out.println("[1] - Nome e idade\n[2] - Somente idade");
+        option = Integer.parseInt(scanner.nextLine());
+
+        switch(option) {
+            case 1:
+                Pessoa pessoaNomeIdade = new Pessoa();
+                System.out.println("Digite o nome: ");
+                pessoaNomeIdade.setNome(scanner.nextLine());
+                System.out.println("Digite a idade: ");
+                pessoaNomeIdade.setIdade(Integer.parseInt(scanner.nextLine()));
+                System.out.println("Nome: " + pessoaNomeIdade.getNome());
+                System.out.println("Idade: " + pessoaNomeIdade.getIdade());
+                break;
+            
+            case 2:
+                Pessoa pessoaIdade = new Pessoa();
+                System.out.println("Digite a idade: ");
+                pessoaIdade.setIdade(Integer.parseInt(scanner.nextLine()));
+                System.out.println("Idade: " + pessoaIdade.getIdade());
+                break;
+
+            default:
+                System.out.println("Opcao invalida");
+                break;
+            }
+        } 
+}
