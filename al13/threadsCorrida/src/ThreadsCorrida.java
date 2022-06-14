d0240780e13c8bafc316c01cf036f7e378450358
Mauricio Pereira <mauriciopereirabraga@gmail.com>
2022-06-14 11:11:29 -0300

    al13

---
+++ al13/threadsCorrida/src/ThreadsCorrida.java
@@ -0,0 +1,58 @@
+/*
+Fazer uma thread para corrida de fórmula 1. Onde vários pilotos
+dão a largada ao mesmo tempo.
+*/
+public class ThreadsCorrida {
+    public static void main(String[] args) {
+        new Thread(t1).start();
+        new Thread(t2).start();
+        new Thread(t3).start();
+    }
+
+    private static Runnable t1 = new Runnable() {
+        @Override
+        public void run() {
+            String nome = "fulano";
+            try {  
+                System.out.println("LARGADA");
+                System.out.println("Primeira volta: " + nome);
+                for(int cont = 0; cont < 10000; cont++) {}
+                System.out.println(nome + " terminou a corrida"); 
+            }
+            
+            catch(Exception e) {
+                
+            }
+        }
+    };
+    
+    private static Runnable t2 = new Runnable() {
+        @Override
+        public void run() {
+            String nome = "ciclano";
+            try {
+                System.out.println("LARGADA");
+                System.out.println("Primeira volta: " + nome);
+                for(int cont = 0; cont < 10000; cont++) {}
+                System.out.println(nome + " terminou a corrida"); 
+            }
+
+            catch(Exception e) {}
+        }
+    };
+
+    private static Runnable t3 = new Runnable() {
+        @Override
+        public void run() {
+            String nome = "beltrano";
+            try {
+                System.out.println("LARGADA");
+                System.out.println("Primeira volta: " + nome);
+                for(int cont = 0; cont < 10000; cont++) {}
+                System.out.println(nome + " terminou a corrida"); 
+            }
+
+            catch(Exception e) {}
+        }
+    };
+}
