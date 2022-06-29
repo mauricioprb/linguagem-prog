236cd77ed8e083c0f80812b3843254a919c8fdbd
mauricioprb <mauriciopereirabraga@gmail.com>
2022-06-28 22:35:09 -0300

    att

--- heranca/carro/src/Carro.java
+++ heranca/carro/src/Carro.java
@@ -1,7 +1,7 @@
 public class Carro extends Veiculo {
     protected static String nomeCarro = "Fusca";
     public static void main(String[] args) {
-        System.out.println("Mau carro é um: ");
+        System.out.println("Meu carro é um: ");
     }
 
     public static String exibirMarca() {
